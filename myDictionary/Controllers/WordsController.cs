61ea53dc1d73a5d009c9391b20c036989800e0a7
Theveenan <theveenan@gmail.com>
2022-01-25 23:31:47 -0500

    finished for night

---
+++ myDictionary/Controllers/WordsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace myDictionary.Controllers
+{
+    [Router("api/words")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        
+    }
+}
